49fc40575b98d5f32710e8f3851f088cf00208b0
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-07-11 15:22:13 +0430

    packages and controllers class added to main module

---
+++ Main/src/main/java/com/library/main/controller/AdminController.java
@@ -0,0 +1,7 @@
+package com.library.main.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/admin")
+public class AdminController {
+}
---
+++ Main/src/main/java/com/library/main/controller/PublisherController.java
@@ -0,0 +1,7 @@
+package com.library.main.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/publisher")
+public class PublisherController {
+}
