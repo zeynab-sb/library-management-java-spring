0a78b55f33d614986d1370bf3bc98b8b7e2a3726
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-07-12 11:50:56 +0430

    adduser api added

--- Main/src/main/java/com/library/main/controller/AdminController.java
+++ Main/src/main/java/com/library/main/controller/AdminController.java
@@ -1,7 +1,31 @@
 package com.library.main.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.library.main.dto.UserRequest;
+import com.library.main.model.User;
+import com.library.main.repository.UserRepository;
+import com.library.main.utils.Mapper;
+import com.sun.xml.bind.v2.TODO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/admin")
+import javax.validation.Valid;
+
+@RestController
+@Validated
+@RequestMapping("/admin")
 public class AdminController {
+    private Mapper mapper = new Mapper();
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    public ResponseEntity<User> addUser(@Valid @RequestBody UserRequest userRequest){
+        //TODO authentication
+            return new ResponseEntity<>(userRepository.save(mapper.toUser(userRequest)), HttpStatus.CREATED);
+    }
+
 }
