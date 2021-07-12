67bca191e58a0dfaef9f40788d5bb2f0dae7f5ce
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-07-12 11:48:41 +0430

    toUser method added to mapper class

---
+++ Main/src/main/java/com/library/main/utils/Mapper.java
@@ -0,0 +1,12 @@
+package com.library.main.utils;
+
+import com.library.main.dto.UserRequest;
+import com.library.main.model.User;
+
+public class Mapper {
+
+    public User toUser(UserRequest userRequest){
+        return new User(userRequest.getUsername(), userRequest.getPassword(), userRequest.getRole());
+    }
+
+}
