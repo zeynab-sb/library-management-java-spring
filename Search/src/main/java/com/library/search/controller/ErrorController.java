fd6271aedd004dc9ef4012977686b7406a04593d
zahradehghanpur <zahradehghanpour>
2021-07-21 14:04:13 +0430

    add exception handling

---
+++ Search/src/main/java/com/library/search/controller/ErrorController.java
@@ -0,0 +1,25 @@
+package com.library.search.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@Validated
+@RequestMapping("/")
+public class ErrorController {
+    @GetMapping("/error")
+    public ModelAndView showError(){
+
+
+        return new ModelAndView("500" );
+    }
+}
